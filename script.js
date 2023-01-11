a7feccf50487fa06a33b744ab45e3d80e1a0534e
Marianavantsi <118997099+Marianavantsi@users.noreply.github.com>
2023-01-11 21:19:19 +0000

    Changed js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 const getIdeaOne =  document.getElementById ("btn-solution");
 const getIdeaTwo =  document.getElementById ("btn-resolution"); 
 const phrases = [ 
-    "Звучить чудово!",
-    "Однозначно потрібно це зробити!",
-     "Я не впевнений, що це гарна ідея!",
-     "Можливо не завтра!",
-     "Всесвітня мережа не знає, що тобі порадити з цим:(",
-     "Подумай ще завтра про це!"
+    "ЗВУЧИТЬ ЧУДОВО!",
+    "ОДНОЗНАЧНО, ПОТРІБНО ЦЕ ЗРОБИТИ",
+     "Я НЕ ВПЕВНЕНИЙ, ЩО ЦЕ ГАРНА ІДЕЯ!",
+     "ММОЖЛИВО НЕ ЗАВТРА!",
+     "НАВІТЬ ВСЕСВІТНЯ МЕРЕЖА НЕ ЗНАЄ, ЩО ТОБІ ПОРАДИТИ:(",
+     "НЕ ДУМАЙ КРАЩЕ ПРО ЦЕ СЬОГОДНІ!"
     ];
     
 
@@ -22,7 +22,7 @@ document.getElementById ("btn-solution").addEventListener ("click", function(){
 document.getElementById ("btn-resolution").addEventListener ("click", function(){
      getIdeaOne.disabled = false;
      getIdeaTwo.disabled = true;
-    alert ("Подумай про питання, на яке хочеш отримати відповідь і натисни ОК")
+    alert ("Подумай про питання, на яке хочеш отримати відповідь! Потім - закрий це повідомлення і уважно прочитай наступне")
     alert (phrases[Math.floor(Math.random()*6)])
 });
 
